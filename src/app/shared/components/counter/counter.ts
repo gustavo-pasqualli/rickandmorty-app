91352e33ac1d67db88a052815d33356afd09deeb
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-03 14:49:56 -0300

    feat: add counter component

---
+++ src/app/shared/components/counter/counter.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.scss'
+})
+export class Counter {
+  @Input() count = 0;
+}
