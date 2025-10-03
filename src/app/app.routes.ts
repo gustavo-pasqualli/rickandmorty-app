f0c9794a50ef7e365a4b506ca8ba469d2abccff2
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-03 14:51:14 -0300

    refactor: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: 'characters',
-  },
-  {
-    path: 'characters',
     loadChildren: () =>
       import('./features/characters/characters.routes').then((c) => c.charactersRoutes),
   },
--- src/app/features/characters/characters.routes.ts
+++ src/app/features/characters/characters.routes.ts
@@ -3,6 +3,11 @@ import { Route } from '@angular/router';
 export const charactersRoutes: Route[] = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'characters',
+  },
+  {
+    path: 'characters',
     loadComponent: () =>
       import('./characters-page/characters-page').then((c) => c.CharactersPage),
   },
