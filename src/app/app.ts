db5a6249f9c132a97b4553c19f2ab204954609c4
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-02 21:42:35 -0300

    chore: add i18n configs

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslatePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('rickandmorty-app');
+
+  private translate = inject(TranslateService);
+
+  constructor() {
+    this.translate.addLangs(['en', 'pt', 'es']);
+    this.translate.setFallbackLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+
+    this.translate.use(
+      browserLang?.match(/en|pt|es/) ? browserLang : 'pt'
+    );
+  }
 }
+
+
