61fae8ffdc5a864ce0a71208a98b38aed88d0443
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-02 22:55:18 -0300

    feat: add favorite characters page component

---
+++ src/app/features/characters/favorite-characters-page/favorite-characters-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-characters-page',
+  imports: [],
+  templateUrl: './favorite-characters-page.html',
+  styleUrl: './favorite-characters-page.scss'
+})
+export class FavoriteCharactersPage {
+
+}
