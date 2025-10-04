87ce5d1b1a0d06ae8edab12e9fb8f1c69a31453c
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-04 16:54:24 -0300

    chore: add akita configs in main ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { persistState } from '@datorama/akita';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const akitaPersist = persistState({
+  key: 'rm-app',
+  include: ['favorite-characters']
+});
+
+if ((import.meta as any).hot) {
+  (import.meta as any).hot.dispose(() => akitaPersist.destroy());
+}
+
+bootstrapApplication(App, appConfig).catch(err => console.error(err));
