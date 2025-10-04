1581e8947f47013607b03765faf73ac205f18151
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-04 00:40:27 -0300

    feat: add character service

---
+++ src/app/core/services/character.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { PaginatedApiResponse } from '../../shared/models/paginated.model';
+import { Character } from '../../shared/models/characters.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+  url = environment.API_URL;
+
+  http = inject(HttpClient);
+
+  getCharacters(params: any): Observable<PaginatedApiResponse<Character>> {
+    console.log(params)
+    return this.http.get<PaginatedApiResponse<Character>>(this.url + `character`, {
+      params
+    });
+  }
+
+  getCharacterById(id: number): Observable<Character> {
+    return this.http.get<Character>(this.url + `character/${id}`);
+  }
+}
