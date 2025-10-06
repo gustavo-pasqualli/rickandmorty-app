180f42ef7e9cd540091158ac012f278c6d281d96
Switch1511 <gustavopasqualli0@gmail.com>
2025-10-05 22:30:24 -0300

    refactor: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadChildren: () =>
-      import('./features/characters/characters.routes').then((c) => c.charactersRoutes),
+    path: 'characters',
+    loadComponent: () =>
+      import('./features/characters/characters-page/characters-page').then((c) => c.CharactersPage),
   },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./features/characters/favorite-characters-page/favorite-characters-page').then((c) => c.FavoriteCharactersPage),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'characters' },
+  { path: '**', redirectTo: 'characters' }
 ];
